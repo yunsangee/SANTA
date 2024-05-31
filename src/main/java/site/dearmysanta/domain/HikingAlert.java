04f90eaac49e167f1198b5128153dc873d811cb6
yunsangee <michael0622@naver.com>
2024-05-31 17:29:25 +0900

    2024.05.31 17:28 getter setter

--- src/main/java/site/dearmysanta/domain/HikingAlert.java
+++ src/main/java/site/dearmysanta/domain/HikingAlert.java
@@ -1,5 +1,24 @@
 package site.dearmysanta.domain;
 
+import java.sql.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString	
+
 public class HikingAlert {
 	
 	private int userNo; //회원식별번호
@@ -11,68 +30,5 @@ public class HikingAlert {
 	private String meetingTime; //모임시간
 	private String alertContent; //알림내용
 	private String allAlert; //전체알림
-	
-	
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	public int getHikingAlerFlag() {
-		return hikingAlerFlag;
-	}
-	public void setHikingAlerFlag(int hikingAlerFlag) {
-		this.hikingAlerFlag = hikingAlerFlag;
-	}
-	public String getDestinationAlert() {
-		return destinationAlert;
-	}
-	public void setDestinationAlert(String destinationAlert) {
-		this.destinationAlert = destinationAlert;
-	}
-	public String getSunsetAlert() {
-		return sunsetAlert;
-	}
-	public void setSunsetAlert(String sunsetAlert) {
-		this.sunsetAlert = sunsetAlert;
-	}
-	public String getLocationOverAlert() {
-		return locationOverAlert;
-	}
-	public void setLocationOverAlert(String locationOverAlert) {
-		this.locationOverAlert = locationOverAlert;
-	}
-	public String getMeetingTimeAlert() {
-		return meetingTimeAlert;
-	}
-	public void setMeetingTimeAlert(String meetingTimeAlert) {
-		this.meetingTimeAlert = meetingTimeAlert;
-	}
-	public String getMeetingTime() {
-		return meetingTime;
-	}
-	public void setMeetingTime(String meetingTime) {
-		this.meetingTime = meetingTime;
-	}
-	public String getAlertContent() {
-		return alertContent;
-	}
-	public void setAlertContent(String alertContent) {
-		this.alertContent = alertContent;
-	}
-	public String getAllAlert() {
-		return allAlert;
-	}
-	public void setAllAlert(String allAlert) {
-		this.allAlert = allAlert;
-	}
-	@Override
-	public String toString() {
-		return "HikingAlert [userNo=" + userNo + ", hikingAlerFlag=" + hikingAlerFlag + ", destinationAlert="
-				+ destinationAlert + ", sunsetAlert=" + sunsetAlert + ", locationOverAlert=" + locationOverAlert
-				+ ", meetingTimeAlert=" + meetingTimeAlert + ", meetingTime=" + meetingTime + ", alertContent="
-				+ alertContent + ", allAlert=" + allAlert + "]";
-	}
 	
 }
--- src/main/java/site/dearmysanta/domain/HikingGuide.java
+++ src/main/java/site/dearmysanta/domain/HikingGuide.java
@@ -3,101 +3,30 @@ package site.dearmysanta.domain;
 import java.sql.Date;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class HikingGuide {
-	private int userNo; //회원식별번호
-	private int hikingRecordNo; //등산기록번호
-	private int mountainTrailNo; //등산로번호
-	private int userDistance; //회원이동거리
-	private int ascentTime; //회원상행시간
-	private int descentTime; //회원하행시간
-	private int totalTime; //총소요시간
-	private Date hikingDate; //등산일자 
-	private List<String> userCoordinates; //회원위치좌표
-	private Mountain mountain; //산정보(mountain class)
-	private Weather weather; //날씨정보(weather class)
-	private Meeting meeting; //모임원식별번호(meeting class)
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	public int getHikingRecordNo() {
-		return hikingRecordNo;
-	}
-	public void setHikingRecordNo(int hikingRecordNo) {
-		this.hikingRecordNo = hikingRecordNo;
-	}
-	public int getMountainTrailNo() {
-		return mountainTrailNo;
-	}
-	public void setMountainTrailNo(int mountainTrailNo) {
-		this.mountainTrailNo = mountainTrailNo;
-	}
-	public int getUserDistance() {
-		return userDistance;
-	}
-	public void setUserDistance(int userDistance) {
-		this.userDistance = userDistance;
-	}
-	public int getAscentTime() {
-		return ascentTime;
-	}
-	public void setAscentTime(int ascentTime) {
-		this.ascentTime = ascentTime;
-	}
-	public int getDescentTime() {
-		return descentTime;
-	}
-	public void setDescentTime(int descentTime) {
-		this.descentTime = descentTime;
-	}
-	public int getTotalTime() {
-		return totalTime;
-	}
-	public void setTotalTime(int totalTime) {
-		this.totalTime = totalTime;
-	}
-	public Date getHikingDate() {
-		return hikingDate;
-	}
-	public void setHikingDate(Date hikingDate) {
-		this.hikingDate = hikingDate;
-	}
-	public List<String> getUserCoordinates() {
-		return userCoordinates;
-	}
-	public void setUserCoordinates(List<String> userCoordinates) {
-		this.userCoordinates = userCoordinates;
-	}
-	public Mountain getMountain() {
-		return mountain;
-	}
-	public void setMountain(Mountain mountain) {
-		this.mountain = mountain;
-	}
-	public Weather getWeather() {
-		return weather;
-	}
-	public void setWeather(Weather weather) {
-		this.weather = weather;
-	}
-	public Meeting getMeeting() {
-		return meeting;
-	}
-	public void setMeeting(Meeting meeting) {
-		this.meeting = meeting;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "hikingGuide [userNo=" + userNo + ", hikingRecordNo=" + hikingRecordNo + ", mountainTrailNo="
-				+ mountainTrailNo + ", userDistance=" + userDistance + ", ascentTime=" + ascentTime + ", descentTime="
-				+ descentTime + ", totalTime=" + totalTime + ", hikingDate=" + hikingDate + ", userCoordinates="
-				+ userCoordinates + "]";
-	}
-	
-	
-	
+    private int userNo; // 회원 식별 번호
+    private int hikingRecordNo; // 등산 기록 번호
+    private int mountainTrailNo; // 등산로 번호
+    private int userDistance; // 회원 이동 거리
+    private int ascentTime; // 회원 상행 시간
+    private int descentTime; // 회원 하행 시간
+    private int totalTime; // 총 소요 시간
+    private Date hikingDate; // 등산 일자
+    private List<String> userCoordinates; // 회원 위치 좌표
+    private Mountain mountain; // 산 정보 (mountain class)
+    private Weather weather; // 날씨 정보 (weather class)
+    private Meeting meeting; // 모임원 식별 번호 (meeting class)
 }
