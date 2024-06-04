aa361562ad29ada83de3d5e27cfd143494156f5a
bitcamp <bitcamp@DESKTOP-6NIUSCS>
2024-06-04 10:15:19 +0900

    2024.06.04 10:12 add CertificationDao, CertificationService, CertificationServiceImpl

---
+++ src/main/java/site/dearmysanta/service/certification/impl/CertificationPostServiceImpl.java
@@ -0,0 +1,41 @@
+package site.dearmysanta.service.certification.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import site.dearmysanta.domain.certificationPost.CertificationPost;
+import site.dearmysanta.service.certification.CertificationPostDao;
+import site.dearmysanta.service.certification.CertificationPostService;
+
+@Service("CertificationServiceImpl")
+public class CertificationPostServiceImpl implements CertificationPostService {
+
+    @Autowired
+    @Qualifier("CertificationPostDao")
+    private CertificationPostDao certificationPostDao;
+
+    @Override
+    public void addCertificationPost(CertificationPost certificationPost) throws Exception {
+        certificationPostDao.addCertificationPost(certificationPost);
+    }
+
+    @Override
+    public CertificationPost getCertificationPost(int certificationPostNo) throws Exception {
+        return certificationPostDao.getCertificationPost(certificationPostNo);
+    }
+
+    @Override
+    public void updateCertificationPost(CertificationPost certificationPost) throws Exception {
+        certificationPostDao.updateCertificationPost(certificationPost);
+    }
+
+    @Override
+    public void deleteCertificationPost(int certificationPostNo) throws Exception {
+        certificationPostDao.deleteCertificationPost(certificationPostNo);
+    }
+
+    @Override
+    public void deleteCertificationPostHashtags(int certificationPostNo) throws Exception {
+        certificationPostDao.deleteHashtagsByPostNo(certificationPostNo);
+    }
+}
