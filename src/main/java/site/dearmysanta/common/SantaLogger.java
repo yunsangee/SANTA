0b79207f70b804a07f0e4e760ca6da1e06fa992e
programsec-bud <ljh98@ajou.ac.kr>
2024-06-14 19:52:54 +0900

    2024.06.14 19:52 fix log4j2 to logback(slf4j)

--- src/main/java/site/dearmysanta/common/SantaLogger.java
+++ src/main/java/site/dearmysanta/common/SantaLogger.java
@@ -1,11 +1,11 @@
 package site.dearmysanta.common;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class SantaLogger {
-	private static final Logger logger = LogManager.getLogger(SantaLogger.class);
+	private static final Logger logger = LoggerFactory.getLogger(SantaLogger.class);
 	
 	
 	public SantaLogger() {
