95d16e555e53f08fc9bfae3b3433b8feb0c183e4
programsec-bud <ljh98@ajou.ac.kr>
2024-06-26 16:45:06 +0900

    2024.06.26 16:45 fix korean encoding in session

--- src/main/java/site/dearmysanta/domain/hikingguide/SecurityConfig.java
+++ src/main/java/site/dearmysanta/domain/hikingguide/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration 
 @EnableWebSecurity
@@ -20,4 +22,5 @@ public class SecurityConfig {
             );
         return http.build();
     }
+    
 }
