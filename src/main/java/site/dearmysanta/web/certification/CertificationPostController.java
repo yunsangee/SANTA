45cd8ca22f375229987518cc4613c2615eb43369
bitcamp <bitcamp@DESKTOP-6NIUSCS>
2024-06-28 20:41:34 +0900

    2024.06.28 20:41 certificationPost

--- src/main/java/site/dearmysanta/web/certification/CertificationPostController.java
+++ src/main/java/site/dearmysanta/web/certification/CertificationPostController.java
@@ -308,7 +308,7 @@ public class CertificationPostController {
         
         model.addAttribute("certificationPostImages", list);
 
-        return "redirect:/certificationPost/getCertificationPost?postNo=" + certificationPost.getPostNo();
+        return "forward:/certificationPost/getCertificationPost?postNo=" + certificationPost.getPostNo();
     }
 
 
