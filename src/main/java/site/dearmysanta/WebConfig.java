8dc71541d0fef21819fe07e4894f91259a43b350
programsec-bud <ljh98@ajou.ac.kr>
2024-06-29 22:55:35 +0900

    cc

--- src/main/java/site/dearmysanta/WebConfig.java
+++ src/main/java/site/dearmysanta/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://example.com")
+                        .allowedOrigins("http://dearmysanta.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
