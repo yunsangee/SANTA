a21f0db2fa761bc7c9c8c97e4394e8d77472f33f
jaeho-github <jaeho914@gmail.com>
2024-07-03 12:48:49 +0900

    2024.07.03 12:48 commit;

--- src/main/java/site/dearmysanta/service/chatting/impl/ChattingServiceImpl.java
+++ src/main/java/site/dearmysanta/service/chatting/impl/ChattingServiceImpl.java
@@ -21,8 +21,8 @@ public class ChattingServiceImpl implements ChattingService {
 //	@Value("${nodejsServerUrl}")
 //	private String nodeJsServerUrl;
 	
-//	private String nodeJsServerUrl = "https://www.dearmysanta.site";
-	private String nodeJsServerUrl = "http://localhost:3002";
+	private String nodeJsServerUrl = "https://www.dearmysanta.site";
+//	private String nodeJsServerUrl = "http://localhost:3002";
 	
 	@Autowired
 	@Qualifier("chattingDAO")
